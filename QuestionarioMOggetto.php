13a9f85466d7c577279204e95a1b08de9e69d47e
Ikokhan99 <federico.costa-2@studneti.unitn.it>
2020-10-20 15:50:01 +0200

    Create QuestionarioMOggetto.php
    commit the questionnaire of Male objectfication by Fede & Mauri
    XOXO

---
+++ QuestionarioMOggetto.php
@@ -0,0 +1,382 @@
+<html lang="en">
+<head>
+    <title>
+        Questionnaire_two
+    </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div style="text-align: center;">
+<form action="ultima.php" method="post">
+<p style= "font-weight: 1000;">
+Here are some statements about the relationship between men and women in society. Please indicate your
+agreement on a scale: from 1 = disagree strongly to 5 = agree strongly.
+ </p>
+<table class="center">
+    <tr>
+        <td> </td>
+        <td style= "font-weight: 1000;">disagree strongly</td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td style= "font-weight: 1000;">agree strongly</td>
+    </tr>
+    <tr>
+    <td>1. When approaching a woman, most men think more about what that
+    women can do to please him than what he can do to please her. </td>
+        <td style="text-align: center">
+            <input type="radio" name="please" value="1" id = "one">
+            <label  for="one"> 
+                1
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="please" value="2" id = "two">
+            <label for="two"> 
+                2
+            </label> 
+        </td>
+        <td style="text-align: center"> 
+        <input type="radio" name="please" value="3" id = "three">
+            <label for="three"> 
+                3
+            </label>
+        </td>
+        <td style="text-align: center">  <input type="radio" name="please" value="4" id = "four">
+            <label for="four"> 
+                4
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="please" value="5" id = "five">
+            <label for="five"> 
+                5
+            </label>
+        </td>
+    </tr>
+    <tr>
+    <td>2. Most men tend to approach a woman only when they want to have
+    sex with her. </td>
+        <td style="text-align: center">
+            <input type="radio" name="approach" value="1" id = "one">
+            <label for="one"> 
+                1
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="approach" value="2" id = "two">
+            <label for="two"> 
+                2
+            </label> 
+        </td>
+        <td style="text-align: center"> 
+        <input type="radio" name="approach" value="3" id = "three">
+            <label for="three"> 
+                3
+            </label>
+        </td>
+        <td style="text-align: center">  
+            <input type="radio" name="approach" value="4" id = "four">
+            <label for="four"> 
+                4
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="approach" value="5" id = "five">
+            <label for="five"> 
+                5
+            </label>
+        </td>
+    </tr>
+    <tr>
+    <td>3. Most men are interested in women’s feelings because they want to
+    be close to women.</td>
+        <td style="text-align: center">
+            <input type="radio" name="interested" value="1" id = "one">
+            <label for="one"> 
+                1
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="interested" value="2" id = "two">
+            <label for="two"> 
+                2
+            </label> 
+        </td>
+        <td style="text-align: center"> 
+        <input type="radio" name="interested" value="3" id = "three">
+            <label for="three"> 
+                3
+            </label>
+        </td>
+        <td style="text-align: center">  
+        <input type="radio" name="interested" value="4" id = "four">
+            <label for="four"> 
+                4
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="interested" value="5" id = "five">
+            <label for="five"> 
+                5
+            </label>
+        </td>
+    </tr>
+    <tr>
+    <td>4. When a man flatters a woman, it is because he wants to have sex
+    with her. </td>
+        <td style="text-align: center">
+            <input type="radio" name="flatters" value="1" id = "one">
+            <label for="one"> 
+                1
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="flatters" value="2" id = "two">
+            <label for="two"> 
+                2
+            </label> 
+        </td>
+        <td style="text-align: center"> 
+        <input type="radio" name="flatters" value="3" id = "three">
+            <label for="three"> 
+                3
+            </label>
+        </td>
+        <td style="text-align: center">  
+        <input type="radio" name="flatters" value="4" id = "four">
+            <label for="four"> 
+                4
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="flatters" value="5" id = "five">
+            <label for="five"> 
+                5
+            </label>
+        </td>
+    </tr>
+    <tr>
+    <td>5. A man is likely to be interested in a woman to the extent to which she
+    can satisfy his sexual appetite. </td>
+        <td style="text-align: center">
+            <input type="radio" name="appetite" value="1" id = "one">
+            <label for="one"> 
+                1
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="appetite" value="2" id = "two">
+            <label for="two"> 
+                2
+            </label> 
+        </td>
+        <td style="text-align: center"> 
+        <input type="radio" name="appetite" value="3" id = "three">
+            <label for="three"> 
+                3
+            </label>
+        </td>
+        <td style="text-align: center">  
+        <input type="radio" name="appetite" value="4" id = "four">
+            <label for="four"> 
+                4
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="appetite" value="5" id = "five">
+            <label for="five"> 
+                5
+            </label>
+        </td>
+    </tr>
+    <tr>
+    <td>6. Most men consider women sexual objects. </td>
+        <td style="text-align: center">
+            <input type="radio" name="objects" value="1" id = "one">
+            <label for="one"> 
+                1
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="objects" value="2" id = "two">
+            <label for="two"> 
+                2
+            </label> 
+        </td>
+        <td style="text-align: center"> 
+        <input type="radio" name="objects" value="3" id = "three">
+            <label for="three"> 
+                3
+            </label>
+        </td>
+        <td style="text-align: center">  
+        <input type="radio" name="objects" value="4" id = "four">
+            <label for="four"> 
+                4
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="objects" value="5" id = "five">
+            <label for="five"> 
+                5
+            </label>
+        </td>
+    </tr>
+    <tr>
+    <td>7.Most relationships between a man and a woman are based on
+    closeness and affection. </td>
+        <td style="text-align: center">
+            <input type="radio" name="relationships" value="1" id = "one">
+            <label for="one"> 
+                1
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="relationships" value="2" id = "two">
+            <label for="two"> 
+                2
+            </label> 
+        </td>
+        <td style="text-align: center"> 
+        <input type="radio" name="relationships" value="3" id = "three">
+            <label for="three"> 
+                3
+            </label>
+        </td>
+        <td style="text-align: center">  
+        <input type="radio" name="relationships" value="4" id = "four">
+            <label for="four"> 
+                4
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="relationships" value="5" id = "five">
+            <label for="five"> 
+                5
+            </label>
+        </td>
+    </tr>
+    <tr>
+    <td>8. When his sexual desire weakens, a man will likely lose interest in a
+    woman. </td>
+        <td style="text-align: center">
+            <input type="radio" name="weakens" value="1" id = "one">
+            <label for="one"> 
+                1
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="weakens" value="2" id = "two">
+            <label for="two"> 
+                2
+            </label> 
+        </td>
+        <td style="text-align: center"> 
+        <input type="radio" name="weakens" value="3" id = "three">
+            <label for="three"> 
+                3
+            </label>
+        </td>
+        <td style="text-align: center">  
+        <input type="radio" name="weakens" value="4" id = "four">
+            <label for="four"> 
+                4
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="weakens" value="5" id = "five">
+            <label for="five"> 
+                5
+            </label>
+        </td>
+    </tr>
+    <tr>
+    <td>9. When it comes to sex, for most men a woman equals another as long
+    as she satisfies his sexual needs. </td>
+        <td style="text-align: center">
+            <input type="radio" name="sex" value="1" id = "one">
+            <label for="one"> 
+                1
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="sex" value="2" id = "two">
+            <label for="two"> 
+                2
+            </label> 
+        </td>
+        <td style="text-align: center"> 
+        <input type="radio" name="sex" value="3" id = "three">
+            <label for="three"> 
+                3
+            </label>
+        </td>
+        <td style="text-align: center">  
+        <input type="radio" name="sex" value="4" id = "four">
+            <label for="four"> 
+                4
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="sex" value="5" id = "five">
+            <label for="five"> 
+                5
+            </label>
+        </td>
+    </tr>
+    <tr>
+    <td>10. Most men have a full consideration of women as persons. </td>
+        <td style="text-align: center">
+            <input type="radio" name="consideration" value="1" id = "one">
+            <label for="one"> 
+                1
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="consideration" value="2" id = "two">
+            <label for="two"> 
+                2
+            </label> 
+        </td>
+        <td style="text-align: center"> 
+        <input type="radio" name="consideration" value="3" id = "three">
+            <label for="three"> 
+                3
+            </label>
+        </td>
+        <td style="text-align: center">  
+        <input type="radio" name="consideration" value="4" id = "four">
+            <label for="four"> 
+                4
+            </label>
+        </td>
+        <td style="text-align: center">
+        <input type="radio" name="consideration" value="5" id = "five">
+            <label for="five"> 
+                5
+            </label>
+        </td>
+    </tr>
+    
+</table>
+</div>
+<style>
+table {
+  border-collapse: collapse;
+  background-color: #F8F8F8;
+}
+
+table, td, th {
+  border: 1px solid black;
+  padding: 15px;
+  text-align: left;
+}
+td {
+  height: 50px;
+  vertical-align: bottom;
+}
+
+</style>
+</body>
+</html>
